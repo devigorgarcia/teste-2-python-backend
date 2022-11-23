f26debbdec12633aac8d38897edd1a2ed463f4f7
Igor Garcia <igorgg@gmail.com>
2022-11-23 10:52:35 -0300

    feat: upload working

--- transactions/views.py
+++ transactions/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 
 # Create your views here.
 def upload(request):
     if request.method == "POST":
-        uploaded_file = request.FILES['document']
-    return render(request, 'upload.html')
-        
-        
-        
+        uploaded_file = request.FILES["document"]
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
+    return render(request, "upload.html")
